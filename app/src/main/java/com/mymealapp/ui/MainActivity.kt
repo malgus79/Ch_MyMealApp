a269ad08c903247ce1cd25aad95a9590ac9c8e37
Gustavo Maldonado <gamcba7982@gmail.com>
2023-02-15 14:51:16 -0300

    Created check connectivity in real times

--- app/src/main/java/com/mymealapp/ui/MainActivity.kt
+++ app/src/main/java/com/mymealapp/ui/MainActivity.kt
@@ -2,15 +2,20 @@ package com.mymealapp.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.google.android.material.snackbar.Snackbar
 import com.mymealapp.R
 import com.mymealapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,12 +23,17 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
 
+    private var booleanState: Boolean? = null
+
+    private val viewModel by viewModels<MainActivityViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupNavigation()
+        checkNetworkConnection()
     }
 
     private fun setupNavigation() {
@@ -51,5 +61,45 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
 
+    }
+
+    private fun checkNetworkConnection() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.isConnected.collectLatest { isConnected ->
+
+                if (booleanState == null && isConnected) {
+                    booleanState = false
+                }
+
+                if (booleanState == false && !isConnected) {
+                    snackBarConnectivityOff()
+                    booleanState = true
+                }
+
+                if (booleanState == true && isConnected) {
+                    snackBarConnectivityOn()
+                    booleanState = false
+                }
+            }
+        }
+    }
+
+    private fun snackBarConnectivityOff() {
+        Snackbar.make(binding.root, R.string.no_connection, Snackbar.LENGTH_INDEFINITE)
+            .setAction(getString(R.string.rule_out)) {}
+            .setAnchorView(binding.bottomNavigationView)
+            .setBackgroundTint(
+                ContextCompat.getColor(this@MainActivity, R.color.red_theme)
+            )
+            .show()
+    }
+
+    private fun snackBarConnectivityOn() {
+        Snackbar.make(binding.root, R.string.connection_restored, Snackbar.LENGTH_SHORT)
+            .setAnchorView(binding.bottomNavigationView)
+            .setBackgroundTint(
+                ContextCompat.getColor(this@MainActivity, R.color.green_dark_theme)
+            )
+            .show()
     }
 }
---
+++ app/src/main/java/com/mymealapp/ui/MainActivityViewModel.kt
@@ -0,0 +1,15 @@
+package com.mymealapp.ui
+
+import androidx.lifecycle.ViewModel
+import com.mymealapp.core.ConnectivityNetwork
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityViewModel @Inject constructor(connectivityNetwork: ConnectivityNetwork) :
+    ViewModel() {
+
+    val isConnected: Flow<Boolean> = connectivityNetwork.isConnected
+
+}
